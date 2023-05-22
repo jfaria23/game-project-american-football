deca77bc873b76f313713e506481796027d00e8e
Joao Faria <joaorafaria@gmail.com>
2023-05-22 12:20:51 +0100

    images  aligned and ready

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+class Player {
+  constructor() {
+    this.positionY = 200;
 
+    this.element.style.position = "absolute";
+    this.element.style.top = this.positionY + "px";
+  }
+}
+
+class Defenders {
+  constructor() {}
+}
